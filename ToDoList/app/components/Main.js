2e97c4e31e14b1c7d10166e11ee40adec772eb10
Tushar <gupta.tus@husky.neu.edu>
2018-10-23 14:35:17 -0400

    Staing od the app and setup done

---
+++ ToDoList/app/components/Main.js
@@ -0,0 +1,29 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {StyleSheet, Text, View, TextInput, ScrollView, TouchableOpacity } from 'react-native';
+
+
+
+type Props = {};
+export default class Main extends Component<Props> {
+  render() {
+    return (
+      <View ><Text>Testinf</Text>
+        
+      </View>
+    );
+  }
+}
+  
+const styles = StyleSheet.create({
+  container: {
+    
+  },
+});
